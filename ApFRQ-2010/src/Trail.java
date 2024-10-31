80b7e919650591a7dc74e5393cf5212c5ce93990
krzys <krzys@DESKTOP-OFH4BAR>
2024-10-31 12:20:14 -0400

    asdwqdw

---
+++ ApFRQ-2010/src/Trail.java
@@ -0,0 +1,19 @@
+
+public class Trail {
+	private int[] markers;
+	
+	public Trail(int[] m) {
+		this.markers = m;		
+	}
+	public boolean isLevelTrailSegment(int start, int end) {
+		
+		
+		
+		
+		
+		return false;
+	}
+	public boolean isDifficult() {
+		return true;
+	}
+}
---
+++ ApFRQ-2010/src/trailTester.java
@@ -0,0 +1,10 @@
+
+public class trailTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] everest= {100,150,105,120,90,80,50,75,75,70,80,90,100};
+		Trail basecamp = new Trail(everest);
+	}
+
+}
