58f1452936e34c8394413166ecffa175aa8ef1f5
krzys <krzys@DESKTOP-OFH4BAR>
2024-10-31 12:37:59 -0400

    asfh

--- ApFRQ-2010/src/Trail.java
+++ ApFRQ-2010/src/Trail.java
@@ -6,13 +6,18 @@ public class Trail {
 		this.markers = m;		
 	}
 	public boolean isLevelTrailSegment(int start, int end) {
-		
-		
-		
-		
-		
+		int change = Math.abs(start - end);
+		if(change >= 10) {
+			return true;
+		}
 		return false;
 	}
+	
+	
+	
+	
+	
+	
 	public boolean isDifficult() {
 		return true;
 	}
