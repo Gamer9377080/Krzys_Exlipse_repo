5663a53498ef1a3b01daa3f38b11b7df8dee0967
krzys <krzys@DESKTOP-OFH4BAR>
2024-12-04 12:55:43 -0500

    a

--- Ap-Frq-2017/src/Phrase.java
+++ Ap-Frq-2017/src/Phrase.java
@@ -1,4 +1,20 @@
 
 public class Phrase {
-
+	private String currentPhrase;
+	
+	public Phrase(String p) {
+		currentPhase =p;
+	}
+	public int findNthOccittence(String str, int n) {
+		
+	}
+	public void replasceNthOccerence(String str, int n, String repl) {
+		
+	}
+	public int findLastOccurrence(String str) {
+		
+	}
+	public String toString(){
+		
+	}
 }
