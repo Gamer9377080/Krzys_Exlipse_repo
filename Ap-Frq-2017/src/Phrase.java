adeacdb11a0116182f41f04b33c0a0c387d50448
krzys <krzys@DESKTOP-OFH4BAR>
2024-12-10 12:58:25 -0500

    afsd

--- Ap-Frq-2017/src/Phrase.java
+++ Ap-Frq-2017/src/Phrase.java
@@ -3,18 +3,51 @@ public class Phrase {
 	private String currentPhrase;
 	
 	public Phrase(String p) {
-		currentPhase =p;
+		this.currentPhrase = p;
 	}
 	public int findNthOccittence(String str, int n) {
-		
+		int index = -1;
+        int count = 0;
+
+        for (int i = 0; i <= currentPhrase.length() - str.length(); i++) {
+            if (currentPhrase.substring(i, i + str.length()).equals(str)) {
+                count++;
+                if (count == n) {
+                    return i;
+                }
+            }
+        }
+        return -1;
 	}
 	public void replasceNthOccerence(String str, int n, String repl) {
-		
+		int index = -1;
+	    int count = 0;
+
+	    for (int i = 0; i <= currentPhrase.length() - str.length(); i++) {
+	        if (currentPhrase.substring(i, i + str.length()).equals(str)) {
+	            count++;
+	            if (count == n) {
+	            }
+	        }
+	    }
 	}
 	public int findLastOccurrence(String str) {
-		
+		 int lastIndex = -1;
+	        int occurrence = 1;
+	        int currentIndex;
+
+	        while ((currentIndex = findNthOccittence(str, occurrence)) != -1) {
+	            lastIndex = currentIndex;
+	            occurrence++;
+	        }
+
+	        return lastIndex;
 	}
 	public String toString(){
+		return this.currentPhrase;
+	}
+	public void replaceNthOccurrence(String string, int i, String string2) {
+		// TODO Auto-generated method stub
 		
 	}
 }
---
+++ Ap-Frq-2017/src/PhraseTester.java
@@ -0,0 +1,9 @@
+
+public class PhraseTester {
+	public static void main(String[] args) {
+        Phrase phrase1 = new Phrase("A cat ate late.");
+        phrase1.replaceNthOccurrence("at", 1, "rane");
+        System.out.println(phrase1); 
+
+	}
+}
