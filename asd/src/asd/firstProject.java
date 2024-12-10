0fd3900b609567e3300da7d3fec35e39cc2d4c5a
krzys <krzys@DESKTOP-OFH4BAR>
2024-12-10 12:22:01 -0500

    asd

--- asd/src/asd/firstProject.java
+++ asd/src/asd/firstProject.java
@@ -6,5 +6,13 @@ public class firstProject {
 	
 	
 	public static void main(String[] args) {
+		int a = 3 + 2 * 3;
+
+int b = 4 + 3 / 2;
+
+int c = 7 % 4 + 3;
+
+double d = a + b + c;
+System.out.println(d);
 	}
 }
