215103497930dc281058d837edc7085d59bc050c
krzys <krzys@DESKTOP-OFH4BAR>
2025-01-28 12:29:35 -0500

    k

--- 2DArray/src/MessyTest.java
+++ 2DArray/src/MessyTest.java
@@ -1,6 +1,11 @@
 public class MessyTest {
 public static void main(String[] args) {
 	int[][] a = new int[3][3];
-	
+	for(int row = 0;row<a.length;row++) {
+		for(int col=0;col<a[row].length;col++) {
+			System.out.print(a[row][col]+" ");
+		}
+		System.out.println();
+	}
 }
 }
