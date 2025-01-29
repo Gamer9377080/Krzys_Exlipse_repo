8d03c74fa1c5131d5ced239b89a624a86c1d53c2
Krzysztof Kowalski <Kwrzos08@gmail.com>
2025-01-28 20:31:17 -0500

    asdf

--- Ap-Frq-2017/src/Digits.java
+++ Ap-Frq-2017/src/Digits.java
@@ -17,8 +17,11 @@ public class Digits {
 		return digitList;
 	}
 	public boolean isStrictlyIncreasing() {
-	 if() {
-		 
-	 }
+		for (int i = 0; i < digitList.size() - 1; i++) {
+			if (digitList.get(i) >= digitList.get(i + 1)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
