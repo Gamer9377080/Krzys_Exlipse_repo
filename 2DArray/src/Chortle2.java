2437863ca0fb06d80b8b10f290cb49d10a35ba8d
krzys <krzys@DESKTOP-OFH4BAR>
2025-01-30 12:51:18 -0500

    path

--- 2DArray/src/Chortle2.java
+++ 2DArray/src/Chortle2.java
@@ -1,31 +1,21 @@
-
 public class Chortle2 {
-	public static void main ( String[] args ) 
-	  {
-	    int[][] data = { {3, 2, 5},
-	                     {1, 4, 4, 8, 13},
-	                     {9, 1, 0, 2},
-	                     {0, 2, 6, 3, -1, -8} };
-	    
-	    // declare the sum
-	    int sum=0;
-	    
-	    // compute the sums for each row
-	    for ( int row=0; row < data.length; row++)
-	    {
-	      // initialize the sum
+	public static int[] rowsums(int[][] data) {
+		int sum=0;
+	    int[] summ = new int[data.length];
+	    for ( int row=0; row < data.length; row++){
 	      sum=0;
-	      
-	      // compute the sum for this row
-	      for ( int col=0; col < data[row].length; col++) 
-	      {
+	      for ( int col=0; col < data[row].length; col++) {
 	    	 sum += data[row][col];
-	         
 	      }
-	      
-	      // write the sum for this row
-	      System.out.println(sum);
+	      summ[row] = sum;
+	      }
+	    return summ;
 	    }
-	      
-	  }
+		
+	public static void main (String[] args ) 
+	  {
+	    int[][] data = { {3, 2, 5},{1, 4, 4, 8, 13},{9, 1, 0, 2},{0, 2, 6, 3, -1, -8} };
+	    int[] summ = rowsums(data);
+	    System.out.print(summ);
+	   }
 }
