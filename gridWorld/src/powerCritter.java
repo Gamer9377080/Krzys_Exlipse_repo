efd4201492a22cbce4121d5e6d2f8649b6678cc3
krzys <krzys@DESKTOP-OFH4BAR>
2025-03-27 12:57:31 -0400

    rq

--- gridWorld/src/powerCritter.java
+++ gridWorld/src/powerCritter.java
@@ -1,31 +1,10 @@
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Critter;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
-public class powerCritter extends Bug{
+public class powerCritter extends Critter{
 	private int hp = 50;
-	public void act() {
-		while(hp>0) {
-			if(canMove()){
-				move();
-			}else {
-				turn();
-			}
-			System.out.print(hp);
-		}
-	}
-	public void move() {
-		Grid<Actor> gr = getGrid();
-		 if (gr == null)
-			 return;
-		 Location loc = getLocation();
-		 Location next = loc.getAdjacentLocation(getDirection());
-		 if (gr.isValid(next)) {
-			 moveTo(next);
-		 } else {
-			 removeSelfFromGrid();
-		 }
-		 hp =- 1;
-	}
+	
  }
